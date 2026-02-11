487f004ec3f14c491395d6d6b11511398134f1cb
warrior-robotics <bhs.robotics@isd181.org>
2026-02-11 15:49:34 -0600

    Add constants and subsystems for fuel system motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,10 @@
 package frc.robot;
 
 public final class Constants {
-
+  public static final int INTAKE_ID = 1;
+  public static final int INTAKE_CONVEYOR_ID = 2;
+  public static final int HOPPER_ID = 3;
+  public static final int FEEDER_ID = 4;
   }
 
-}
+

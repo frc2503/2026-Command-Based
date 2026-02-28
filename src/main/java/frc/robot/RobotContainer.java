8625db27a000d36af947b001f451787ce65cdbda
Zane Jacobi <zane05jacobi@gmail.com>
2026-02-28 15:29:52 -0600

    comment out code for testing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,27 +14,27 @@ public class RobotContainer {
   private final CommandXboxController driverController = new CommandXboxController(DRIVER_CONTROLLER_PORT);
 
   // The operator's controller
-  private final CommandXboxController operatorController = new CommandXboxController(OPERATOR_CONTROLLER_PORT);
+  // private final CommandXboxController operatorController = new CommandXboxController(OPERATOR_CONTROLLER_PORT);
 
   private final VisionSubsystem visionSubsystem = new VisionSubsystem();
 
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem(visionSubsystem);
   private final SwerveCommand swerveCommand = new SwerveCommand(swerveSubsystem, () -> driverController.getLeftX(), () -> -driverController.getLeftY(), () -> -driverController.getRightX(), () -> true);
-  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  private final HopperSubsystem hopperSubsystem = new HopperSubsystem();
+  // private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  // private final HopperSubsystem hopperSubsystem = new HopperSubsystem();
   //private final LaunchSubsystem towerSubsystem = new LaunchSubsystem();
 
   public RobotContainer() {
     configureBindings();
 
     // Default command for hopper to keep it running at a low speed to prevent binding
-    hopperSubsystem.setDefaultCommand(
-      Commands.run(() -> hopperSubsystem.setHopper(HOPPER_VOLTAGE * 0.1), hopperSubsystem)
-    );
+    // hopperSubsystem.setDefaultCommand(
+    //   Commands.run(() -> hopperSubsystem.setHopper(HOPPER_VOLTAGE * 0.1), hopperSubsystem)
+    // );
   }
   private void configureBindings() {
-      operatorController.leftBumper().onTrue(new ToggleIntakeArm(intakeSubsystem));
-      operatorController.rightTrigger().whileTrue(new FuelCommand(intakeSubsystem, hopperSubsystem));
+      // operatorController.leftBumper().onTrue(new ToggleIntakeArm(intakeSubsystem));
+      // operatorController.rightTrigger().whileTrue(new FuelCommand(intakeSubsystem, hopperSubsystem));
       //operatorController.rightBumper().whileTrue(new LaunchCommand(towerSubsystem));
       //operatorController.leftTrigger().whileTrue(new ReverseFuelCommand(intakeSubsystem, hopperSubsystem, towerSubsystem));
   }

d4c413ab96b450b8b8996716cd56c2a5d2019417
Zane Jacobi <zane05jacobi@gmail.com>
2026-03-04 22:50:16 -0600

    print encoder position

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -15,6 +15,7 @@ import static edu.wpi.first.units.Units.Rotation;
 import static frc.robot.Constants.IntakeConstants.*;
 
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.spark.SparkMax;
@@ -39,7 +40,7 @@ public class IntakeSubsystem extends SubsystemBase {
         intakeArmConfig.closedLoop.allowedClosedLoopError(INTAKE_ARM_ALLOWED_ERROR.in(Rotation), ClosedLoopSlot.kSlot0);
         intakeArmConfig.closedLoop.feedbackSensor(FeedbackSensor.kAbsoluteEncoder);
         intakeArmConfig.absoluteEncoder.setSparkMaxDataPortConfig();
-        intakeArmConfig.absoluteEncoder.zeroOffset(INTAKE_ARM_ENCODER_OFFSET);
+        //intakeArmConfig.absoluteEncoder.zeroOffset(INTAKE_ARM_ENCODER_OFFSET);
         // intakeArmConfig.absoluteEncoder.startPulseUs(3.884);
         // intakeArmConfig.absoluteEncoder.endPulseUs(998.06);
         intakeArmMotor.configure(intakeArmConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
@@ -89,5 +90,10 @@ public class IntakeSubsystem extends SubsystemBase {
 
     public double getAbsoluteEncoderPosition() {
         return intakeArmAbsoluteEncoder.getPosition();
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Encoder", getAbsoluteEncoderPosition());
     }
 }
